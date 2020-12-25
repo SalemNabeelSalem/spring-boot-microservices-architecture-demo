7a1a3d81bb2002aca615ddbc63aebbd7018d6613
Salem Nabeel Salem <samnabeelsam@outlook.com>
2020-12-25 19:31:04 +0300

    creating the configuration-service

---
+++ configurations-service/src/test/java/com/springbootmicroservices/configurationsservice/ConfigurationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.configurationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
