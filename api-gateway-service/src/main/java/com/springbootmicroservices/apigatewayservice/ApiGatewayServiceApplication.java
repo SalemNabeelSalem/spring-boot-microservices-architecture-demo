ab71dfb2aba10b3dbdc83f924518ddf254db677d
Salem Nabeel Salem <samnabeelsam@outlook.com>
2020-12-25 17:51:24 +0300

    adding the hystrix circuit breaker and configure it on the zull gate way api service

--- api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,7 +3,9 @@ package com.springbootmicroservices.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
---
+++ api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/FallBackMethodController.java
@@ -0,0 +1,22 @@
+package com.springbootmicroservices.apigatewayservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+
+        return "User Service Is Taking Longer Than Expected." +
+                " Please Try Again Later.";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+
+        return "Department Service Is Taking Longer Than Expected." +
+                " Please Try Again Later.";
+    }
+}
