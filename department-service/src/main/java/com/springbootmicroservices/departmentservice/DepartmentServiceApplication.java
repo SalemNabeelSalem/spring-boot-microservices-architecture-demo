e654ca292fdbec27914f61b06944df84dbb51285
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-11 17:58:30 +0300

    configure the departments microservice to be an eureka client

--- department-service/src/main/java/com/springbootmicroservices/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/springbootmicroservices/departmentservice/DepartmentServiceApplication.java
@@ -2,9 +2,9 @@ package com.springbootmicroservices.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
