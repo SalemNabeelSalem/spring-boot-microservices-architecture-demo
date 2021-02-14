507afe09174a0e18444f9b8ffbc93d4616a6571a
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-14 18:29:49 +0300

    create the service-administrator spring-boot admin server

---
+++ service-administrator/src/main/java/com/springbootmicroservices/serviceadministrator/ServiceAdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.serviceadministrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAdministratorApplication.class, args);
+	}
+
+}
---
+++ service-administrator/src/test/java/com/springbootmicroservices/serviceadministrator/ServiceAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.serviceadministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
