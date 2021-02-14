7db5b0cd76fee49e775ba777842d9cc87161bb1d
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-14 20:26:32 +0300

    configure the service-administrator with other services

--- service-administrator/src/main/java/com/springbootmicroservices/serviceadministrator/ServiceAdministratorApplication.java
+++ service-administrator/src/main/java/com/springbootmicroservices/serviceadministrator/ServiceAdministratorApplication.java
@@ -1,13 +1,19 @@
 package com.springbootmicroservices.serviceadministrator;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceAdministratorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceAdministratorApplication.class, args);
 	}
-
 }
