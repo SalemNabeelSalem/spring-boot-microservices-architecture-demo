99fbc8a1365babe872df2ed515628009b069635f
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-22 21:38:06 +0300

    configure the resource-service to working as part of microservice architecture

--- resource-service/src/main/java/com/springbootmicroservices/resourceservice/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/springbootmicroservices/resourceservice/ResourceServiceApplication.java
@@ -2,12 +2,14 @@ package com.springbootmicroservices.resourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ResourceServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ResourceServiceApplication.class, args);
 	}
-
 }
