7f4ebeefd4f05ced7114dcf282b599f242befe92
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-22 21:40:30 +0300

    change configuration properties on api-gateway-service and on user-service and make some recovery on department-service

--- api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,9 +3,10 @@ package com.springbootmicroservices.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableHystrix
+// @EnableHystrix
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
