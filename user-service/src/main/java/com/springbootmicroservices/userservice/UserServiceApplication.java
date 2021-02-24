894c5efb55745343a6e90dec83b5f34c14231df8
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-24 22:01:32 +0300

    make some refactoring on user-service

--- user-service/src/main/java/com/springbootmicroservices/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/springbootmicroservices/userservice/UserServiceApplication.java
@@ -2,10 +2,7 @@ package com.springbootmicroservices.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -14,12 +11,5 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced // for using the service-name (department-service) instead of ip-address (localhost:9001)
-	public RestTemplate restTemplate() {
-
-		return new RestTemplate();
 	}
 }
