4e944a7f451517953a5467aee97ac5a9bae0ba5e
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-24 22:37:43 +0300

    make some refactoring the department-service

--- department-service/src/main/java/com/springbootmicroservices/departmentservice/controllers/DepartmentController.java
+++ department-service/src/main/java/com/springbootmicroservices/departmentservice/controllers/DepartmentController.java
@@ -12,13 +12,13 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
-    @PostMapping("/save_new_department")
+    @PostMapping(value = "/save_new_department")
     public Department saveNewDepartment(@RequestBody Department InputDepartment) {
 
         return departmentService.saveNewDepartment(InputDepartment);
     }
 
-    @GetMapping("/get_department_by_id/{id}")
+    @GetMapping(value = "/get_department_by_id/{id}")
     public Department getDepartmentById(@PathVariable("id") Long id) {
 
         return departmentService.getDepartmentById(id);
