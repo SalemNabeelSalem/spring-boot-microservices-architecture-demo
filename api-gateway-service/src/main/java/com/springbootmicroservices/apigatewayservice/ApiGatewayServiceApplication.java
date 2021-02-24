c8dfcf8be06ed9d9ef04a07ac53f504073457b8a
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-02-24 23:15:07 +0300

    create and configure the zuul-gateway-service

--- api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/springbootmicroservices/apigatewayservice/ApiGatewayServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-// @EnableHystrix
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
